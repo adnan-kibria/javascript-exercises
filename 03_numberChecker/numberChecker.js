e11132510efa4a0d0cf52a4571e854e2f76a5147
adnan-kibria <adnan.official.rb000@gmail.com>
2025-10-02 21:54:45 +0600

    Complete code of exercise 3

--- 03_numberChecker/numberChecker.js
+++ 03_numberChecker/numberChecker.js
@@ -1,10 +1,13 @@
 function numberChecker(number) {
-  if (number === 6) {
+  if (number >= 10) {
     return true;
   } else {
     return false;
   }
 }
-
+numberChecker(1000);
+numberChecker(10);
+numberChecker(9);
+numberChecker(6);
 // Do not edit below this line
 module.exports = numberChecker;
