6e3a4a6e2c119c8bb765678025f91d39a7e425c5
adnan-kibria <adnan.official.rb000@gmail.com>
2025-10-25 14:02:59 +0600

    Repeating string exercised is solved

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let string = '';
+    for(let i = 0; i<num; i++){
+        string += str;
+    }
+    if(num<0) string = 'ERROR';
+    return string;
 };
+repeatString('hey',3);
+repeatString('hello',10);
+repeatString('hi',1);
+repeatString('bye',0);
+repeatString('goodbye', -1);
+repeatString('',10);
 
 // Do not edit below this line
 module.exports = repeatString;
