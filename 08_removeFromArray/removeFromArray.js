96a4d90ed72eaf3ec5d50111a0a95c9ef718f9db
adnan-kibria <adnan.official.rb000@gmail.com>
2025-10-25 16:09:04 +0600

    Alt - Modified test cases by removing skip keyword

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...element) {
+    const newArray = array.filter(
+        (item) => !element.includes(item)
+    )
+    return newArray;
 };
 
 // Do not edit below this line
